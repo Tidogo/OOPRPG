7312d6485d6cfa19aa1cc533cad99d63c5652785
Timothy Goddard <jediknightjaden@msn.com>
2016-02-09 18:31:26 -0800

    Minor UI changes
    Minor UI changes for the inventory menu to increase readability. Version
    .1 of stats menu added.

--- OOPRPG/Inventory.Designer.cs
+++ OOPRPG/Inventory.Designer.cs
@@ -32,11 +32,6 @@
             this.lbx_Backpack = new System.Windows.Forms.ListBox();
             this.lbl_Backpack = new System.Windows.Forms.Label();
             this.lbl_Equiped = new System.Windows.Forms.Label();
-            this.lbl_Head = new System.Windows.Forms.Label();
-            this.lbl_Chest = new System.Windows.Forms.Label();
-            this.lbl_Legs = new System.Windows.Forms.Label();
-            this.lbl_RightHand = new System.Windows.Forms.Label();
-            this.lbl_LeftHand = new System.Windows.Forms.Label();
             this.cbx_Head = new System.Windows.Forms.ComboBox();
             this.cbx_Chest = new System.Windows.Forms.ComboBox();
             this.cbx_RightHand = new System.Windows.Forms.ComboBox();
@@ -46,13 +41,14 @@
             this.txt_Cash = new System.Windows.Forms.TextBox();
             this.btn_Exit = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // lbx_Backpack
             // 
             this.lbx_Backpack.FormattingEnabled = true;
-            this.lbx_Backpack.Location = new System.Drawing.Point(327, 28);
+            this.lbx_Backpack.Location = new System.Drawing.Point(327, 21);
             this.lbx_Backpack.Name = "lbx_Backpack";
             this.lbx_Backpack.Size = new System.Drawing.Size(125, 290);
             this.lbx_Backpack.TabIndex = 0;
@@ -60,70 +56,27 @@
             // lbl_Backpack
             // 
             this.lbl_Backpack.AutoSize = true;
-            this.lbl_Backpack.Location = new System.Drawing.Point(360, 9);
+            this.lbl_Backpack.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Backpack.Location = new System.Drawing.Point(324, 3);
             this.lbl_Backpack.Name = "lbl_Backpack";
-            this.lbl_Backpack.Size = new System.Drawing.Size(56, 13);
+            this.lbl_Backpack.Size = new System.Drawing.Size(60, 15);
             this.lbl_Backpack.TabIndex = 1;
             this.lbl_Backpack.Text = "Backpack";
             // 
             // lbl_Equiped
             // 
             this.lbl_Equiped.AutoSize = true;
+            this.lbl_Equiped.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lbl_Equiped.Location = new System.Drawing.Point(12, 9);
             this.lbl_Equiped.Name = "lbl_Equiped";
-            this.lbl_Equiped.Size = new System.Drawing.Size(46, 13);
+            this.lbl_Equiped.Size = new System.Drawing.Size(67, 16);
             this.lbl_Equiped.TabIndex = 3;
-            this.lbl_Equiped.Text = "Equiped";
-            // 
-            // lbl_Head
-            // 
-            this.lbl_Head.AutoSize = true;
-            this.lbl_Head.Location = new System.Drawing.Point(130, 40);
-            this.lbl_Head.Name = "lbl_Head";
-            this.lbl_Head.Size = new System.Drawing.Size(33, 13);
-            this.lbl_Head.TabIndex = 4;
-            this.lbl_Head.Text = "Head";
-            // 
-            // lbl_Chest
-            // 
-            this.lbl_Chest.AutoSize = true;
-            this.lbl_Chest.Location = new System.Drawing.Point(130, 106);
-            this.lbl_Chest.Name = "lbl_Chest";
-            this.lbl_Chest.Size = new System.Drawing.Size(34, 13);
-            this.lbl_Chest.TabIndex = 5;
-            this.lbl_Chest.Text = "Chest";
-            // 
-            // lbl_Legs
-            // 
-            this.lbl_Legs.AutoSize = true;
-            this.lbl_Legs.Location = new System.Drawing.Point(134, 232);
-            this.lbl_Legs.Name = "lbl_Legs";
-            this.lbl_Legs.Size = new System.Drawing.Size(30, 13);
-            this.lbl_Legs.TabIndex = 6;
-            this.lbl_Legs.Text = "Legs";
-            // 
-            // lbl_RightHand
-            // 
-            this.lbl_RightHand.AutoSize = true;
-            this.lbl_RightHand.Location = new System.Drawing.Point(22, 165);
-            this.lbl_RightHand.Name = "lbl_RightHand";
-            this.lbl_RightHand.Size = new System.Drawing.Size(61, 13);
-            this.lbl_RightHand.TabIndex = 7;
-            this.lbl_RightHand.Text = "Right Hand";
-            // 
-            // lbl_LeftHand
-            // 
-            this.lbl_LeftHand.AutoSize = true;
-            this.lbl_LeftHand.Location = new System.Drawing.Point(209, 165);
-            this.lbl_LeftHand.Name = "lbl_LeftHand";
-            this.lbl_LeftHand.Size = new System.Drawing.Size(54, 13);
-            this.lbl_LeftHand.TabIndex = 8;
-            this.lbl_LeftHand.Text = "Left Hand";
+            this.lbl_Equiped.Text = "Equipped";
             // 
             // cbx_Head
             // 
             this.cbx_Head.FormattingEnabled = true;
-            this.cbx_Head.Location = new System.Drawing.Point(84, 56);
+            this.cbx_Head.Location = new System.Drawing.Point(97, 63);
             this.cbx_Head.Name = "cbx_Head";
             this.cbx_Head.Size = new System.Drawing.Size(121, 21);
             this.cbx_Head.TabIndex = 9;
@@ -132,7 +85,7 @@
             // cbx_Chest
             // 
             this.cbx_Chest.FormattingEnabled = true;
-            this.cbx_Chest.Location = new System.Drawing.Point(84, 122);
+            this.cbx_Chest.Location = new System.Drawing.Point(97, 119);
             this.cbx_Chest.Name = "cbx_Chest";
             this.cbx_Chest.Size = new System.Drawing.Size(121, 21);
             this.cbx_Chest.TabIndex = 10;
@@ -141,7 +94,7 @@
             // cbx_RightHand
             // 
             this.cbx_RightHand.FormattingEnabled = true;
-            this.cbx_RightHand.Location = new System.Drawing.Point(1, 181);
+            this.cbx_RightHand.Location = new System.Drawing.Point(15, 156);
             this.cbx_RightHand.Name = "cbx_RightHand";
             this.cbx_RightHand.Size = new System.Drawing.Size(121, 21);
             this.cbx_RightHand.TabIndex = 11;
@@ -151,7 +104,7 @@
             // cbx_LeftHand
             // 
             this.cbx_LeftHand.FormattingEnabled = true;
-            this.cbx_LeftHand.Location = new System.Drawing.Point(174, 181);
+            this.cbx_LeftHand.Location = new System.Drawing.Point(185, 156);
             this.cbx_LeftHand.Name = "cbx_LeftHand";
             this.cbx_LeftHand.Size = new System.Drawing.Size(121, 21);
             this.cbx_LeftHand.TabIndex = 12;
@@ -160,7 +113,7 @@
             // cbx_Legs
             // 
             this.cbx_Legs.FormattingEnabled = true;
-            this.cbx_Legs.Location = new System.Drawing.Point(84, 248);
+            this.cbx_Legs.Location = new System.Drawing.Point(81, 223);
             this.cbx_Legs.Name = "cbx_Legs";
             this.cbx_Legs.Size = new System.Drawing.Size(121, 21);
             this.cbx_Legs.TabIndex = 13;
@@ -169,15 +122,16 @@
             // lbl_Cash
             // 
             this.lbl_Cash.AutoSize = true;
-            this.lbl_Cash.Location = new System.Drawing.Point(171, 299);
+            this.lbl_Cash.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Cash.Location = new System.Drawing.Point(182, 296);
             this.lbl_Cash.Name = "lbl_Cash";
-            this.lbl_Cash.Size = new System.Drawing.Size(31, 13);
+            this.lbl_Cash.Size = new System.Drawing.Size(39, 16);
             this.lbl_Cash.TabIndex = 14;
             this.lbl_Cash.Text = "Cash";
             // 
             // txt_Cash
             // 
-            this.txt_Cash.Location = new System.Drawing.Point(221, 296);
+            this.txt_Cash.Location = new System.Drawing.Point(221, 291);
             this.txt_Cash.Name = "txt_Cash";
             this.txt_Cash.ReadOnly = true;
             this.txt_Cash.Size = new System.Drawing.Size(100, 20);
@@ -198,12 +152,22 @@
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-11, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 13);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(306, 283);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 17;
             this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 24);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Stats";
             // 
             // Inventory
             // 
@@ -211,7 +175,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.ClientSize = new System.Drawing.Size(464, 321);
+            this.ClientSize = new System.Drawing.Size(464, 556);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_Exit);
             this.Controls.Add(this.txt_Cash);
             this.Controls.Add(this.lbl_Cash);
@@ -220,11 +185,6 @@
             this.Controls.Add(this.cbx_RightHand);
             this.Controls.Add(this.cbx_Chest);
             this.Controls.Add(this.cbx_Head);
-            this.Controls.Add(this.lbl_LeftHand);
-            this.Controls.Add(this.lbl_RightHand);
-            this.Controls.Add(this.lbl_Legs);
-            this.Controls.Add(this.lbl_Chest);
-            this.Controls.Add(this.lbl_Head);
             this.Controls.Add(this.lbl_Equiped);
             this.Controls.Add(this.lbl_Backpack);
             this.Controls.Add(this.lbx_Backpack);
@@ -243,11 +203,6 @@
         private System.Windows.Forms.ListBox lbx_Backpack;
         private System.Windows.Forms.Label lbl_Backpack;
         private System.Windows.Forms.Label lbl_Equiped;
-        private System.Windows.Forms.Label lbl_Head;
-        private System.Windows.Forms.Label lbl_Chest;
-        private System.Windows.Forms.Label lbl_Legs;
-        private System.Windows.Forms.Label lbl_RightHand;
-        private System.Windows.Forms.Label lbl_LeftHand;
         private System.Windows.Forms.ComboBox cbx_Head;
         private System.Windows.Forms.ComboBox cbx_Chest;
         private System.Windows.Forms.ComboBox cbx_RightHand;
@@ -257,5 +212,6 @@
         private System.Windows.Forms.TextBox txt_Cash;
         private System.Windows.Forms.Button btn_Exit;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
